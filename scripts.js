12e8358daf112f36c50a54ff2a6b37d529253e68
Juliet <agutu@gmail.com>
2022-01-04 11:50:47 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+var moringaStudent ={
+    firstName: "Juliet",
+    lastName: "Oluoch",
+    level: 1,
+    track: ["prep", "JavaScript", "Python", "Django"],
+    enrollmentStatus: true
+}
+
+
+
+var myCat = {
+    name: "kitty kanyau",    
+    speak: function() {
+       console.log("meow"); 
+    }
+  }; 
